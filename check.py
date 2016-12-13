93cc1da68b120ce7e0ca5cdcb74a7c51a9c93b26
Dotzer <dotza65@gmail.com>
2016-12-14 10:51:44 +1100

    modified to write into todo.txt for debian task list

--- check.py
+++ check.py
@@ -9,6 +9,8 @@ passfile = open('/home/dotza/python/base/password', 'r')
 passw = passfile.readlines()
 passw = base64.b64decode(passw[0].rstrip())
 
+todo_path = '/home/dotza/.local/share/todo.txt/todo.txt'
+exit_point = 'Check finished, terminate screen\n'
 # Initializing ZabbixAPI
 zapi = ZabbixAPI("http://monitor.st65.ru")
 zapi.login("i.kim", passw)
@@ -20,7 +22,7 @@ def check_subnet(subnet):
     # spliting input into 4 octets
     octets = subnet.split('.')
     result = ''
-    for x in range(2, 255):
+    for x in range(2, 254):
         ip = octets[0] + '.' + octets[1] + '.' + octets[2] + '.' + str(x)
         # pinging host
         if os.system('ping -c 2 -W 1 ' + ip + ' > /dev/null') == 0:
@@ -46,11 +48,19 @@ def check_subnet(subnet):
                            " in Zabbix!\n")
     return result
 
+
+with open('/home/dotza/python/zabbix_check/sub','r') as f1:
+    inp = f1.readlines()
 # Check number of arguments
-if len(sys.argv) > 1:
+if len(inp) > 1:
     # Run Function Check_subnet() for each subnet in arguments
-    for ind in range(1, len(sys.argv)):
-        print 'checking subnet ' + str(sys.argv[ind])
-        print check_subnet(str(sys.argv[ind]))
+    for ind in range(1, len(inp)):
+        print 'checking subnet ' + str(inp[ind])
+        get_res = check_subnet(str(inp[ind]))
+        print get_res
+        with open(todo_path, "a") as myfile:
+            myfile.write(get_res + "\n")
+    with open(todo_path, "a") as myfile:
+        myfile.write(exit_point)
 else:
-    print "There is no argument"
+    print "File is empty"
